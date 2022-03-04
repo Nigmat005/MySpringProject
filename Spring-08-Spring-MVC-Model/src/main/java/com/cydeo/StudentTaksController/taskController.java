391c174e03856ae7c80ed9268ed747632500c8a4
Nigmat005 <Nigmathalmamat@gmail.com>
2022-03-03 20:27:25 -0500

    Changed Array to use list to store StudentTask object

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/StudentTaksController/taskController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/StudentTaksController/taskController.java
@@ -6,17 +6,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class taskController {
     @RequestMapping("/StudentInfoTable")
     public String task(Model model){
+        List<StudentTask> list=new ArrayList<>();
         StudentTask task1= new StudentTask("Mickael","Smith",25, Gender.MALE);
         StudentTask task2= new StudentTask("Tom","Hanks",35, Gender.MALE);
         StudentTask task3= new StudentTask("Ammy","Bryan",33, Gender.FEMALE);
 
-        model.addAttribute("row1",task1);
-        model.addAttribute("row2",task2);
-        model.addAttribute("row3",task3);
+        list.addAll(Arrays.asList(task1,task2,task3));
+
+
+        model.addAttribute("StudentList",list);
 
         return "/StudentTask/studentInfo";
     }
